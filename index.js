51626974ecd02bbf41cdf256f0f47c6ad863c200
Sari <sari.vogel.94@gmail.com>
2020-08-25 23:06:45 +0200

    Set up the environment and the basic chat app

---
+++ index.js
@@ -0,0 +1,27 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
+});
+
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' });
+io.on('connection', (socket) => {
+    socket.broadcast.emit('hi');
+});
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
